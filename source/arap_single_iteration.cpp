5c707767da79d7403dd973a5121503642c2c2261
Rikin <dolllyamar@gmail.com>
2020-10-26 23:55:08 -0400

    Implemented arap_single_iteration, still have bugs but probably in precompute

--- source/arap_single_iteration.cpp
+++ source/arap_single_iteration.cpp
@@ -8,5 +8,14 @@ void arap_single_iteration(
   const Eigen::MatrixXd & bc,
   Eigen::MatrixXd & U)
 {
-  // REPLACE WITH YOUR CODE
+  Eigen::MatrixXd C = K.transpose() * U;
+  Eigen::MatrixXd R(C.rows(), C.cols());
+  for (int k = 0; k < U.rows(); k++) {
+    Eigen::Matrix3d Ck = C.block<3, 3>(k * 3, 0);
+    Eigen::Matrix3d Rk;
+    igl::polar_svd3x3(Ck, Rk);
+    R.block<3, 3>(k * 3, 0) = Rk;
+  }
+  Eigen::MatrixXd Beq = Eigen::MatrixXd::Zero(bc.rows(), bc.cols());
+  igl::min_quad_with_fixed_solve(data, K * R, bc, Beq, U);
 }
