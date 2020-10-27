d18b4fe88330c393bffaa2c37c37f2786e44cd3a
Rikin <dolllyamar@gmail.com>
2020-10-27 02:26:24 -0400

    Implemented ARAP properly

--- source/arap_precompute.cpp
+++ source/arap_precompute.cpp
@@ -9,35 +9,38 @@ void arap_precompute(
         const Eigen::VectorXi &b,
         igl::min_quad_with_fixed_data<double> &data,
         Eigen::SparseMatrix<double> &K) {
+  data.n = V.rows();
   // (negative) cotangent laplacian
   Eigen::SparseMatrix<double> L;
   igl::cotmatrix(V, F, L);
   L *= -1;
   // empty constraints matrix, irrelevant to us
   Eigen::SparseMatrix<double> Aeq(b.rows(), V.rows());
-  igl::min_quad_with_fixed_precompute(L, b, Aeq, true, data);
   // precompute system
-  // cotangents for each angle in triangle
+  igl::min_quad_with_fixed_precompute(L, b, Aeq, true, data);
+
+  // cotangents for each angle in each triangle
   Eigen::MatrixXd C;
   igl::cotmatrix_entries(V, F, C);
 
-  // calculate K using formula in handout
+  // build K matrix
   K.resize(V.rows(), V.rows() * 3);
-  std::vector<Eigen::Triplet<double>> tripletList;
-  for (int f = 0; f < F.rows(); f++) {
-    for (int i = 0; i < 3; i++) {
+  std::vector<Eigen::Triplet<double>> tl;
+  for (int f = 0; f < F.rows(); f++) {  // iterate over faces
+    for (int i = 0; i < 3; i++) {  // iterate over edges
       int vi = F(f, i);
       int vj = F(f, (i + 1) % 3);
-      int vo = F(f, (i + 2) % 3);  // vertex opposite to ij edge
-      for (int k = 0; k < 3; k++) {
+      int opposite = (i + 2) % 3;  // vertex opposite from ij edge
+      // e_ij = cot(opposite) * (V(i) - V(j)) / 3
+      Eigen::Vector3d e_ij = C(f, opposite) * (V.row(vi) - V.row(vj)) / 3;
+      for (int k = 0; k < 3; k++) {  // iterate over vertices
         int vk = F(f, k);
-        Eigen::Vector3d e_ij = C(f, vo) * V.row(vi) - V.row(vj);
         for (int beta = 0; beta < 3; beta++) {
-          tripletList.emplace_back(vi, 3 * vk + beta, e_ij(beta));
-          tripletList.emplace_back(vk, 3 * vk + beta, -e_ij(beta));
+          tl.emplace_back(vi, vk * 3 + beta, e_ij(beta));
+          tl.emplace_back(vj, vk * 3 + beta, -e_ij(beta));
         }
       }
     }
   }
-  K.setFromTriplets(tripletList.begin(), tripletList.end());
+  K.setFromTriplets(tl.begin(), tl.end());
 }
--- source/arap_single_iteration.cpp
+++ source/arap_single_iteration.cpp
@@ -2,20 +2,43 @@
 #include <igl/polar_svd3x3.h>
 #include <igl/min_quad_with_fixed.h>
 
+void closest_rotation(const Eigen::Matrix3d &M, Eigen::Matrix3d &R);
+
 void arap_single_iteration(
-  const igl::min_quad_with_fixed_data<double> & data,
-  const Eigen::SparseMatrix<double> & K,
-  const Eigen::MatrixXd & bc,
-  Eigen::MatrixXd & U)
-{
+        const igl::min_quad_with_fixed_data<double> &data,
+        const Eigen::SparseMatrix<double> &K,
+        const Eigen::MatrixXd &bc,
+        Eigen::MatrixXd &U) {
+  // i'm really not sure why this is so slow D:
   Eigen::MatrixXd C = K.transpose() * U;
-  Eigen::MatrixXd R(C.rows(), C.cols());
-  for (int k = 0; k < U.rows(); k++) {
-    Eigen::Matrix3d Ck = C.block<3, 3>(k * 3, 0);
+  // find rotations closest to blocks of C
+  Eigen::MatrixXd R(C.rows(), 3);
+  for (int k = 0; k < C.rows(); k += 3) {
     Eigen::Matrix3d Rk;
-    igl::polar_svd3x3(Ck, Rk);
-    R.block<3, 3>(k * 3, 0) = Rk;
+    closest_rotation(C.block<3, 3>(k, 0), Rk);
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        R(k + i, j) = Rk(i, j);  // copy Rk to R
+      }
+    }
   }
-  Eigen::MatrixXd Beq = Eigen::MatrixXd::Zero(bc.rows(), bc.cols());
-  igl::min_quad_with_fixed_solve(data, K * R, bc, Beq, U);
+  // empty constraints values, irrelevant to us
+  Eigen::MatrixXd Beq(bc.rows(), bc.cols());
+  // have to solve with B = -KR, probably because we negated L earlier
+  igl::min_quad_with_fixed_solve(data, -K * R, bc, Beq, U);
+}
+
+// find closest rotation to given matrix using singular value decomposition
+void closest_rotation(const Eigen::Matrix3d &M, Eigen::Matrix3d &R) {
+  // find SVD of M to get rotations
+  Eigen::JacobiSVD<Eigen::Matrix3d> svd(M, Eigen::ComputeFullV | Eigen::ComputeFullU);
+  const Eigen::Matrix3d &U = svd.matrixU();
+  const Eigen::Matrix3d &V = svd.matrixV();
+  // optimal omega has 1, 1, det(UV^T) on diagonal
+  Eigen::Matrix3d omega_star = Eigen::Matrix3d::Identity();
+  // det(UV^T) = det(U)det(V^T) = det(U)det(V)
+  omega_star(2, 2) = U.determinant() * V.determinant();
+  // compute R based on optimization formula
+  // return transpose to match use in later computations
+  R = (U * omega_star * V.transpose()).transpose();
 }
